f55112a34ded5b6bbeb2ac66a33ad1445b9159c8
Guilherme Neto <gui.pereiraneto@hotmail.com>
2026-09-14 23:50:52 -0300

    alterado design home page

--- ControleDeEstoque.WebApp/Compartilhado/HomeController.cs
+++ ControleDeEstoque.WebApp/Compartilhado/HomeController.cs
@@ -1,11 +1,24 @@
+using ControleDeEstoque.WebApp.ModuloProdutos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeEstoque.WebApp.Compartilhado;
 
 public class HomeController : Controller
 {
+    private readonly RepositorioProdutoEmArquivo repositorioProduto;
+
+    public HomeController(RepositorioProdutoEmArquivo repositorioProduto)
+    {
+        this.repositorioProduto = repositorioProduto;
+    }
+
     public ActionResult Index()
     {
+        List<Produto> produtos = repositorioProduto.SelecionarTodos();
+
+        ViewBag.TotalProdutos = produtos.Count;
+        ViewBag.TotalEstoque = produtos.Sum(p => p.QuantidadeEmEstoque);
+
         return View();
     }
 }
